c2b9b3989b334945a4a60c3ae5f2e4a1d51ef83d
tq <smarttq@163.com>
2017-08-18 06:46:32 +0000

    flask test

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-import logging; logging.basicConfig(level=logging.INFO)
+from flask import Flask, request, render_template
 
-import asyncio, os, json, time
-from datetime import datetime
+app = Flask(__name__)
 
-from aiohttp import web
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
 
-def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return render_template('form.html')
 
-@asyncio.coroutine
-def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '192.168.100.187', 19000)
-    logging.info('server started at http://192.168.100.187:19000...')
-    return srv
+@app.route('/signin', methods=['POST'])
+def signin():
+    username = request.form['username']
+    password = request.form['password']
+    if username=='admin' and password=='password':
+        return render_template('signin-ok.html', username=username)
+    return render_template('form.html', message='Bad username or password', username=username)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
