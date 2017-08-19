786a373079ffd15c767a199261f5cd17fd693d58
tq <smarttq@163.com>
2017-08-19 08:38:39 +0000

    update delete method

--- test_models.py
+++ test_models.py
@@ -3,27 +3,45 @@ from models import User, Blog, Comment
 import asyncio
 
 async def connecDB(loop):
-	username = 'www-data'
-	password = '1qaz!QAZ'
-	dbname = 'awesome'
-	await orm.create_pool(loop,user=username,password=password,db=dbname)
+    username = 'www-data'
+    password = '1qaz!QAZ'
+    dbname = 'awesome'
+    await orm.create_pool(loop,user=username,password=password,db=dbname)
 async def destoryDB():
-	await orm.destory_pool()
-async def test(loop):
-	await orm.create_pool(loop, user='www-data', password='1qaz!QAZ', db='awesome')
-	u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
-	await u.save()
-	await destoryDB()
+    await orm.destory_pool()
+async def test_insert(loop):
+    await orm.create_pool(loop, user='www-data', password='1qaz!QAZ', db='awesome')
+    u = User(name='bb', email='bb@example.com', passwd='1234567890', image='about:blank')
+    await u.save()
+    await destoryDB()
+
+async def test_findAll(loop):
+    await connecDB(loop)
+    users = await User.findAll('name','bb')
+    for user in users:
+        print('user:%s' % user)
+    await destoryDB()
+
+async def test_find(loop):
+    await connecDB(loop)
+    user = await User.find('001503048923603070f16deb0fc465b877b11036f74e150000')
+    print('user:%s' % user)
+    await destoryDB()
 	
 async def test_delete(loop):
-	await connecDB(loop)
-	user = await User.find('001502529538645353e48f753284c5793a251ca290eb420000')
-	if user is not None:
-		await user.remove()
-		print('user remove:%s' % user)
-	await destoryDB()
+    await connecDB(loop)
+    users = await User.findAll('name','Test')
+    print('find users:%s' % users)
+    if users is not None:
+        for user in users:
+            await user.remove()
+            print('user remove:%s' % user)
+    await destoryDB()
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(test(loop))
-#loop.run_until_complete(test_delete(loop))
-loop.close()
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    #loop.run_until_complete(test_insert(loop))
+    #loop.run_until_complete(test_find(loop))
+    loop.run_until_complete(test_findAll(loop))
+    #loop.run_until_complete(test_delete(loop))
+    loop.close()
